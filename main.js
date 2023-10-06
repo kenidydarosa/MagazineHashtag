598e243a9755909f4ed1534a7853917aeee4834f
Kenidy <kenidyy@hotmail.com>
2023-10-06 18:55:43 -0300

    att

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ import { initializeBag } from "./src/menuCar.js";
 import { datasheet, productGenerate } from "./src/datasheet.js";
 import { menuAlignleft } from "./src/menuListFilters.js";
 import { addFilter } from "./src/menuListFilters.js";
+import { findProduct } from "./src/find.js";
 
 productGenerate(datasheet);
 initializeBag();
 menuAlignleft();
 addFilter();
+findProduct();
